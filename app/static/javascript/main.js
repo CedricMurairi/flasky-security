b90b5471dc118aed46c8622bfe9794c2b94df931
CedricMurairi <murairicedric@gmail.com>
2023-10-13 20:31:12 +0200

    a few JS handlers

---
+++ app/static/javascript/main.js
@@ -0,0 +1,59 @@
+function checkPasswordValidity() {
+    const passwordInput = document.querySelector('#password')
+    const passwordRequirements = document.querySelectorAll('.password-requirement')
+
+    if (!passwordInput) {
+        return
+    }
+
+    passwordInput.addEventListener('input', () => {
+        const passwordValue = passwordInput.value
+
+        passwordRequirements.forEach((requirement, index) => {
+            if (checkRequirement(passwordValue, index)) {
+                requirement.style.color = 'green'
+            } else {
+                requirement.style.color = 'red'
+            }
+        })
+    })
+
+    function checkRequirement(password, index) {
+        switch (index) {
+            case 0:
+                return password.length >= 8
+            case 1:
+                return /[A-Z]/.test(password)
+            case 2:
+                return /[a-z]/.test(password)
+            case 3:
+                return /\d/.test(password)
+            case 4:
+                return /[!@#$%^&*]/.test(password)
+            default:
+                return false
+        }
+    }
+}
+
+checkPasswordValidity()
+
+function toggleReplyForm(commentID) {
+    const replyForm = document.querySelector(`#comment-form-${commentID}`)
+
+    if (replyForm.style.display === 'block') {
+        replyForm.style.display = 'none';
+        return;
+    }
+    replyForm.style.display = 'block';
+}
+
+function toggleRepliesBlock(commentID) {
+    const replyForm = document.querySelector(`#replies-block-${commentID}`)
+
+    if (replyForm.style.display === 'block') {
+        replyForm.style.display = 'none';
+        return;
+    }
+    replyForm.style.display = 'block';
+}
